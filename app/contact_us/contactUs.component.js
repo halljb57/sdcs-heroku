51d05d07f83410a9ef64a5d1f732a436c3398994
Joseph Hall <halljb57@gmail.com>
2016-04-27 13:53:00 -0400

    Add contact us folder and files

---
+++ app/contact_us/contactUs.component.js
@@ -0,0 +1,38 @@
+System.register(['angular2/core'], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+        return c > 3 && r && Object.defineProperty(target, key, r), r;
+    };
+    var __metadata = (this && this.__metadata) || function (k, v) {
+        if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+    };
+    var core_1;
+    var ContactUsComponent;
+    return {
+        setters:[
+            function (core_1_1) {
+                core_1 = core_1_1;
+            }],
+        execute: function() {
+            ContactUsComponent = (function () {
+                function ContactUsComponent() {
+                }
+                ContactUsComponent = __decorate([
+                    core_1.Component({
+                        selector: 'contact_us',
+                        templateUrl: '../app/contact_us/contactUs.component.html'
+                    }), 
+                    __metadata('design:paramtypes', [])
+                ], ContactUsComponent);
+                return ContactUsComponent;
+            }());
+            exports_1("ContactUsComponent", ContactUsComponent);
+        }
+    }
+});
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNvbnRhY3RfdXMvY29udGFjdFVzLmNvbXBvbmVudC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7Ozs7Ozs7OztZQU1BO2dCQUFBO2dCQUFrQyxDQUFDO2dCQUpuQztvQkFBQyxnQkFBUyxDQUFDO3dCQUNQLFFBQVEsRUFBRSxZQUFZO3dCQUN0QixXQUFXLEVBQUUsNENBQTRDO3FCQUM1RCxDQUFDOztzQ0FBQTtnQkFDZ0MseUJBQUM7WUFBRCxDQUFsQyxBQUFtQyxJQUFBO1lBQW5DLG1EQUFtQyxDQUFBIiwiZmlsZSI6ImNvbnRhY3RfdXMvY29udGFjdFVzLmNvbXBvbmVudC5qcyIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7Q29tcG9uZW50fSBmcm9tICdhbmd1bGFyMi9jb3JlJztcclxuXHJcbkBDb21wb25lbnQoe1xyXG4gICAgc2VsZWN0b3I6ICdjb250YWN0X3VzJyxcclxuICAgIHRlbXBsYXRlVXJsOiAnLi4vYXBwL2NvbnRhY3RfdXMvY29udGFjdFVzLmNvbXBvbmVudC5odG1sJ1xyXG59KVxyXG5leHBvcnQgY2xhc3MgQ29udGFjdFVzQ29tcG9uZW50IHsgfSJdfQ==
