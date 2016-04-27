5d3c893732b4de62de59daeccd86c90ef9952573
Joseph Hall <halljb57@gmail.com>
2016-04-27 13:51:05 -0400

    Add about folder and files

---
+++ app/about/about.component.js
@@ -0,0 +1,38 @@
+System.register(['angular2/core'], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+        return c > 3 && r && Object.defineProperty(target, key, r), r;
+    };
+    var __metadata = (this && this.__metadata) || function (k, v) {
+        if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+    };
+    var core_1;
+    var AboutComponent;
+    return {
+        setters:[
+            function (core_1_1) {
+                core_1 = core_1_1;
+            }],
+        execute: function() {
+            AboutComponent = (function () {
+                function AboutComponent() {
+                }
+                AboutComponent = __decorate([
+                    core_1.Component({
+                        selector: 'about',
+                        templateUrl: '../app/about/about.component.html'
+                    }), 
+                    __metadata('design:paramtypes', [])
+                ], AboutComponent);
+                return AboutComponent;
+            }());
+            exports_1("AboutComponent", AboutComponent);
+        }
+    }
+});
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFib3V0L2Fib3V0LmNvbXBvbmVudC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7Ozs7Ozs7OztZQU1BO2dCQUFBO2dCQUE4QixDQUFDO2dCQUovQjtvQkFBQyxnQkFBUyxDQUFDO3dCQUNQLFFBQVEsRUFBRSxPQUFPO3dCQUNqQixXQUFXLEVBQUUsbUNBQW1DO3FCQUNuRCxDQUFDOztrQ0FBQTtnQkFDNEIscUJBQUM7WUFBRCxDQUE5QixBQUErQixJQUFBO1lBQS9CLDJDQUErQixDQUFBIiwiZmlsZSI6ImFib3V0L2Fib3V0LmNvbXBvbmVudC5qcyIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7Q29tcG9uZW50fSBmcm9tICdhbmd1bGFyMi9jb3JlJztcclxuXHJcbkBDb21wb25lbnQoe1xyXG4gICAgc2VsZWN0b3I6ICdhYm91dCcsXHJcbiAgICB0ZW1wbGF0ZVVybDogJy4uL2FwcC9hYm91dC9hYm91dC5jb21wb25lbnQuaHRtbCdcclxufSlcclxuZXhwb3J0IGNsYXNzIEFib3V0Q29tcG9uZW50IHsgfSJdfQ==
