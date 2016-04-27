21c041c38edde00fa7cabbf1f1b5ee3e51eb5a1e
Joseph Hall <halljb57@gmail.com>
2016-04-27 13:53:54 -0400

    Add login folder and files

---
+++ app/login/login.component.js
@@ -0,0 +1,38 @@
+System.register(['angular2/core'], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+        return c > 3 && r && Object.defineProperty(target, key, r), r;
+    };
+    var __metadata = (this && this.__metadata) || function (k, v) {
+        if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+    };
+    var core_1;
+    var LoginComponent;
+    return {
+        setters:[
+            function (core_1_1) {
+                core_1 = core_1_1;
+            }],
+        execute: function() {
+            LoginComponent = (function () {
+                function LoginComponent() {
+                }
+                LoginComponent = __decorate([
+                    core_1.Component({
+                        selector: 'login',
+                        templateUrl: '../app/login/login.component.html'
+                    }), 
+                    __metadata('design:paramtypes', [])
+                ], LoginComponent);
+                return LoginComponent;
+            }());
+            exports_1("LoginComponent", LoginComponent);
+        }
+    }
+});
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImxvZ2luL2xvZ2luLmNvbXBvbmVudC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7Ozs7Ozs7OztZQU1BO2dCQUFBO2dCQUE4QixDQUFDO2dCQUovQjtvQkFBQyxnQkFBUyxDQUFDO3dCQUNQLFFBQVEsRUFBRSxPQUFPO3dCQUNqQixXQUFXLEVBQUUsbUNBQW1DO3FCQUNuRCxDQUFDOztrQ0FBQTtnQkFDNEIscUJBQUM7WUFBRCxDQUE5QixBQUErQixJQUFBO1lBQS9CLDJDQUErQixDQUFBIiwiZmlsZSI6ImxvZ2luL2xvZ2luLmNvbXBvbmVudC5qcyIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7Q29tcG9uZW50fSBmcm9tICdhbmd1bGFyMi9jb3JlJztcclxuXHJcbkBDb21wb25lbnQoe1xyXG4gICAgc2VsZWN0b3I6ICdsb2dpbicsXHJcbiAgICB0ZW1wbGF0ZVVybDogJy4uL2FwcC9sb2dpbi9sb2dpbi5jb21wb25lbnQuaHRtbCdcclxufSlcclxuZXhwb3J0IGNsYXNzIExvZ2luQ29tcG9uZW50IHsgfSJdfQ==
